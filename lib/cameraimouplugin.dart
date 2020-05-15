c8124aab67553465e44690448fe8fbc33ed5bfc4
YourName <limeng@honggv.cn>
2020-05-15 10:31:25 +0800

    。

--- lib/cameraimouplugin.dart
+++ lib/cameraimouplugin.dart
@@ -12,19 +12,17 @@ class Cameraimouplugin {
   }
 
   ///绑定摄像头
-  static Future<String> loginAndBind(String deviceId, String token) async {
-    final String version = await _channel.invokeMethod(
+  static void loginAndBind(String deviceId, String token) async {
+    await _channel.invokeMethod(
         'bind_camera', <String, dynamic>{
       'deviceId': deviceId,
       'token': token
     });
-    return version;
   }
 
   ///解绑摄像头
-  static Future<String> unBindDevice(String deviceId, String token) async {
-    final String version = await _channel.invokeMethod('un_bind_camera',
+  static void unBindDevice(String deviceId, String token) async {
+    await _channel.invokeMethod('un_bind_camera',
         <String, dynamic>{'deviceId': deviceId, 'token': token});
-    return version;
   }
 }
